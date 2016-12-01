70c237a613e3abc09c5c9cda0820105ade2ea4f2
hassey13 <ehasselbring@gmail.com>
2016-12-01 16:38:19 -0500

    Push to xfer

--- lib/song.rb
+++ lib/song.rb
@@ -1,3 +1,5 @@
+require 'pry'
+
 class Song
   attr_accessor :name, :artist_name
   @@all = []
@@ -8,6 +10,43 @@ class Song
 
   def save
     self.class.all << self
+    self
   end
+
+  def self.create
+  	self.new.save
+  end
+
+  def self.new_by_name(name)
+  	song = self.new
+  	song.name = name
+  	song
+  end
+
+  def self.create_by_name(name)
+  	song = self.new_by_name(name)
+  	song.save
+  end
+
+  def self.find_by_name(name)
+  	self.all.detect { |song| song.name == name }
+  end
+
+  def self.find_or_create_by_name(name)
+  	self.find_by_name(name)? self.find_by_name(name) : self.create_by_name(name)
+  end
+
+  def self.alphabetical
+  	song = self.all.collect {|song| song.name}.sort
+
+  	binding.pry
+  	
+  	song.collect {|song| song.name}.sort
+
+  end
+
+
+
+
 
 end
